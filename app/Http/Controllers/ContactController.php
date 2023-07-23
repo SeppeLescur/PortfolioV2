33c183407f6cad7e6f903f49582093cea2882fe4
Seppe Lescur <seppelescur.work@gmail.com>
2023-07-23 14:35:25 +0200

    added comfirmation basics

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -20,8 +20,15 @@ class ContactController extends Controller
             'email' => $request->input('email'),
             'message' => $request->input('message')
         ];
+        $dataComfirmation = [
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'message' => $request->input('message'),
+            'comfirmation' => true
+        ];
 
         Mail::to('seppelescur.work@gmail.com')->send(new ContactFormMail($data));
+        Mail::to($request->input('email'))->send(new ContactFormMail($dataComfirmation));
 
         return redirect()->back()->with('success', 'Email sent successfully!');
     }
--- resources/views/emails/contact-form.blade.php
+++ resources/views/emails/contact-form.blade.php
@@ -2,3 +2,7 @@
 <p><strong>Name:</strong> {{ $data['name'] }}</p>
 <p><strong>Email:</strong> {{ $data['email'] }}</p>
 <p><strong>Message:</strong> {{ $data['message'] }}</p>
+
+@if(isset($data['comfirmation']))
+  Did someone use your email-address? notify us!
+@endif
